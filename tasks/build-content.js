3b95a775a35f3a539f982421cd77db530de912b0
vshulev <velizar.shulev@gmail.com>
2016-01-24 22:44:56 +0200

    Finish build-content task

--- tasks/build-content.js
+++ tasks/build-content.js
@@ -3,9 +3,9 @@ var json = require('jsonfile');
 var mark = require('marky-mark');
 
 module.exports = function() {
-  //return gulp.src('interviews/**/*.png')
-  //  .pipe(gulp.dest('build/images/interviews'));
-
   var interviews = mark.parseDirectorySync(__dirname + '/../interviews');
   json.writeFileSync('build/interviews.json', interviews);
+
+  return gulp.src('interviews/**/*.png')
+    .pipe(gulp.dest('build/images/interviews'));
 };
