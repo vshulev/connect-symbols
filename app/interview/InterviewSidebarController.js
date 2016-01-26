c2445ffd210bd4169fe36ce705a85d9946993c69
vshulev <velizar.shulev@gmail.com>
2016-01-26 22:05:27 +0200

    Add name and description to person sidebar

--- app/interview/InterviewSidebarController.js
+++ app/interview/InterviewSidebarController.js
@@ -5,4 +5,8 @@ function InterviewSidebarController(interview) {
   this.image = {};
   this.image.name = interview.meta.photo;
   this.image.src = `images/people/${this.image.name}.png`;
+
+  this.person = {};
+  this.person.name = interview.meta.name;
+  this.person.description = interview.meta.description;
 }
