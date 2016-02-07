5d3c3db6f4715a4d09dd1ece64edb0ae809de6bf
vshulev <velizar.shulev@gmail.com>
2016-02-07 13:06:07 +0000

    Add top separator

--- app/ui/separator/index.js
+++ app/ui/separator/index.js
@@ -2,6 +2,7 @@ const angular = require('angular');
 const name = 'connect-symbols.ui.separator';
 
 import { Separator } from './Separator';
+import { SeparatorTop } from './SeparatorTop';
 import { DynamicSeparator } from './DynamicSeparator';
 
 module.exports = name;
@@ -9,4 +10,5 @@ module.exports = name;
 angular.module(name, [])
 
 .directiveClass('separator', Separator)
+.directiveClass('separatorTop', SeparatorTop)
 .directiveClass('dynamicSeparator', DynamicSeparator);
