adc5afd1b2e44e82363a45802233e89a8a72bcc1
vshulev <velizar.shulev@gmail.com>
2016-02-07 14:27:52 +0000

    Add hover functionality

--- app/ui/navigation/index.js
+++ app/ui/navigation/index.js
@@ -3,10 +3,12 @@ const name = 'connect-symbols.ui.navigation';
 
 import { SideNavigation } from './SideNavigation';
 import { SideNavigationDynamic } from './SideNavigationDynamic';
+import { SideNavigationHiding } from './SideNavigationHiding';
 
 module.exports = name;
 
 angular.module(name, [])
 
 .directiveClass('sideNavigation', SideNavigation)
-.directiveClass('dynamicSideNavigation', SideNavigationDynamic);
+.directiveClass('dynamicSideNavigation', SideNavigationDynamic)
+.directiveClass('hidingSideNavigation', SideNavigationHiding);
