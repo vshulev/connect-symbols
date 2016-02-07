73c2b4e1376aa7e900fcdbaa9660cf4b771b57ce
vshulev <velizar.shulev@gmail.com>
2016-02-07 15:35:35 +0000

    Fix top logo

--- app/ui/logo/LogoTop.js
+++ app/ui/logo/LogoTop.js
@@ -8,7 +8,7 @@ export class LogoTop extends Directive {
     this.replace = true;
     this.template = `
       <div class="text-center">
-        <div move-up top="-5rem" class="cs-logo-top"></div>
+        <div move-up top="-8rem" class="cs-logo-top"></div>
       </div>
     `;
   }
